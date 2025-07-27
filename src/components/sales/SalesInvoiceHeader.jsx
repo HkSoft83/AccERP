a8229b7be70c276e1e29e81cff61d094510c6955
HkSoft83 <hkbookkeepers@gmail.com>
2025-07-27 20:41:16 +0600

    sales invoice error fix

--- src/components/sales/SalesInvoiceHeader.jsx
+++ src/components/sales/SalesInvoiceHeader.jsx
@@ -79,7 +79,7 @@ const SalesInvoiceHeader = ({
                 <SelectValue placeholder={saleType === 'cash' ? "Cash Customer (Optional)" : "Select customer"} />
               </SelectTrigger>
               <SelectContent>
-                {customers.map(c => (<SelectItem key={c.id} value={c.id}>{c.name}</SelectItem>))}
+                {customers?.map(c => (<SelectItem key={c.id} value={c.id}>{c.name}</SelectItem>))}
               </SelectContent>
             </Select>
           </div>
